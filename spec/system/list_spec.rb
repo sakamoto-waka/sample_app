dd832c28fadfea9a87578a7b4713202ad5027f73
sakamoto-waka <wakanmhl@gmail.com>
2022-03-21 14:07:04 +0000

    [fix] (1)root_path → top_path

--- spec/system/list_spec.rb
+++ spec/system/list_spec.rb
@@ -6,7 +6,7 @@ describe '投稿のテスト' do
   let!(:list) { create(:list, title:'hoge', body:'body') }
   describe 'トップ画面(top-path)のテスト' do
     before do
-      visit root_path
+      visit top_path
     end
     context '表示の確認' do
       it 'トップ画面(top_path)に「ここはTopページです」が表示されている' do
